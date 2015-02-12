0a96c0233382a73b3297746e11498f084245561b
David Kroell <dkroell@acm.org>
2015-02-12 14:39:49 -0500

    Updating php for Database configurations widely distro...

--- mysql server/config.php
+++ mysql server/config.php
@@ -1,10 +1,16 @@
+
+/*
+	The configuration is a construst of the clearDB system we have online.
+	Centralized Database system is held on Sequel Pro.
+*/
+
 <?php
 define("SERVERPORT","3306");
 
-define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "1234");
-define("DATABASE","");
+define("HOST", "us-mm-demo-dca-01.cleardb.com");
+define("USER", "44538038239161");
+define("PASSWORD", "86177d67");
+define("DATABASE","cdb_eval_e893385480b5af7");
 
 define("SECURE", FALSE);
 ?>
--- mysql server/sqlfunc.js
+++ mysql server/sqlfunc.js
@@ -1,28 +1,44 @@
 
 var SERVER_PORT 	= 3306;
 
-var mysql = require('mysql');
+var connection = require('mysql');
 
-var MYSQL_DEFAULT_HOST 	= 'localhost';	// define address of mysql server
+var MYSQL_DEFAULT_HOST 	= '127.0.0.1';	// define address of mysql server
 var MYSQL_DEFAULT_PASS	= '1234';			// define password for mysql server
-var MYSQL_DEFAULT_DB 	= 'mydb';		// define default mysql database name
+var MYSQL_DEFAULT_DB 	= 'ToneMe';		// define default mysql database name
 var MYSQL_DEFAULT_USER	= 'root';		// define username for mysql server
 
 
-  	var connection = mysql.createConnection({
+   connection = connection.createConnection({
     	host     : MYSQL_DEFAULT_HOST,
     	user     : MYSQL_DEFAULT_USER,
     	password : MYSQL_DEFAULT_PASS,
     	database : MYSQL_DEFAULT_DB
   	});
 
+
     connection.connect(function(err) {
-       console.log(err);
+      if(err) {
+        return console.log(err);
+      }
+      // do anything here after successfully connecting to the server
+  console.log('Connected to the mysql server');
+
     });
 
-    // do anything here after successfully connecting to the server
-	console.log('Connected to the mysql server');
 
-	function doSampleQuery() {
-		mysqlServer.query("SELECT * FROM `sample_table`");
-	}
+  function loginLogic(userName, pass) {
+     if (userName.indexOf("'") == -1 && pass.indexOf("'") == -1) {
+      var queryString = 'SELECT * FROM Users WHERE password = ' +  + '';
+      var retVal = connection.query(queryString, function(error, rows, columns) {
+          console.log(rows);
+      });
+      return retVal;
+    }
+    else {
+      return null;
+    }
+  }
+
+loginLogic("Bob", "1234");
+
