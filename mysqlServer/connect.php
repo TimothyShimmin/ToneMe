317db603b5ec71dbbbc26003309e345dc42b8bc2
Joseph Whitaker <joseph.whitaker@forrent.com>
2015-02-12 20:01:50 -0500

    testing

--- mysqlServer/connect.php
+++ mysqlServer/connect.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'config.php';
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE)
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+print_r($mysqli);
  or die("Unable to connect to server on $server");
 ?>
